fb0a5bbdb792a5b9bb2166bfcd3a38bd7b5c19c9
Edward Medrano <edwardgino13@gmail.com>
2025-10-07 20:25:19 -0500

    Create project in maven

---
+++ src/main/java/com/edward/school/EdwardSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.edward.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdwardSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdwardSchoolApplication.class, args);
+	}
+
+}
